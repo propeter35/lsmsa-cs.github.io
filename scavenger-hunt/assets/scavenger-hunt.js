86be98c2f43a319e58ee356d0c25450a39d94707
Isoscelestial <isoscelestial@gmail.com>
2024-09-28 14:24:19 -0500

    Added functionality to tab bar

--- scavenger-hunt/assets/scavenger-hunt.js
+++ scavenger-hunt/assets/scavenger-hunt.js
@@ -1,3 +1,5 @@
+// --- COLOR SCHEME ---
+
 // let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 document.querySelectorAll("#color-scheme-switcher").forEach((element) => {
@@ -8,3 +10,17 @@ function toggleColorTheme() {
   colorScheme = colorScheme == "dark" ? "light" : "dark";
   updateColorScheme();
 }
+
+// --- TAB BAR ---
+
+document.querySelectorAll(".scahoo-tab:not(.disabled)").forEach((element) => {
+  element.addEventListener("click", function (e) {
+    if (this.getAttribute("href")) {
+      window.location.href = this.getAttribute("href");
+    } else {
+      if (confirm("Uh oh, this tab does not exist! Would you like to return to the main Scavenger Hunt page?")) {
+        window.location.href = "/scavenger-hunt";
+      }
+    }
+  });
+});
