ca96999f61195e0907b88e57a678a41e7b84c9b0
Isoscelestial <isoscelestial@gmail.com>
2024-09-28 14:35:16 -0500

    Accessibility improvement: Turned tabs into anchors instead of divs

--- scavenger-hunt/assets/scavenger-hunt.js
+++ scavenger-hunt/assets/scavenger-hunt.js
@@ -13,12 +13,13 @@ function toggleColorTheme() {
 
 // --- TAB BAR ---
 
+// not necessary anymore, can be removed
 document.querySelectorAll(".scahoo-tab:not(.disabled)").forEach((element) => {
   element.addEventListener("click", function (e) {
     if (this.getAttribute("href")) {
       window.location.href = this.getAttribute("href");
     } else {
-      if (confirm("Uh oh, this tab does not exist! Would you like to return to the main Scavenger Hunt page?")) {
+      if (confirm("Uh oh, this tab hasn't been setup! Would you like to return to the main Scavenger Hunt page?")) {
         window.location.href = "/scavenger-hunt";
       }
     }
